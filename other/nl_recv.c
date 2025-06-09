ae4868af1d6b6fa4f802d87c8226b84b069eaec9
dgiantsidi <dimitra.giantsidi@gmail.com>
2025-06-09 12:55:44 +0000

    works finally

---
+++ other/nl_recv.c
@@ -0,0 +1,106 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <sys/socket.h>
+#include <linux/netlink.h>
+
+#define MAX_PAYLOAD 1024  /* maximum payload size */
+#define NETLINK_TEST 17
+
+
+static char message[MAX_PAYLOAD];
+
+static char* get_message(void)
+{  
+  static int counter = 0; // static to retain value between calls
+  snprintf(message, MAX_PAYLOAD, "%d", counter);
+  counter++;
+  return message;
+}
+
+int main(int argc, char **argv)
+{
+  struct sockaddr_nl src_addr;
+  struct sockaddr_nl dest_addr;
+  struct nlmsghdr *nlh;
+  struct msghdr msg;
+  struct iovec iov;
+  int sock_fd;
+  int rc;
+
+  if (argc != 2) {
+    printf("usage: %s <message>\n", argv[0]);
+    return 1;
+  }
+
+  sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_TEST);
+  if (sock_fd < 0) {
+    printf("socket: %s\n", strerror(errno));
+    return 1;
+  }
+
+  memset(&src_addr, 0, sizeof(src_addr));
+  src_addr.nl_family = AF_NETLINK;
+  src_addr.nl_pid = getpid();  /* self pid */
+  src_addr.nl_groups = 0;  /* not in mcast groups */
+  bind(sock_fd, (struct sockaddr*)&src_addr, sizeof(src_addr));
+
+
+
+   for (;;) {
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    dest_addr.nl_family = AF_NETLINK;
+    dest_addr.nl_pid = 0;   /* For Linux Kernel */
+    dest_addr.nl_groups = 0; /* unicast */
+
+    nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
+
+    /* Fill the netlink message header */
+    nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
+    nlh->nlmsg_pid = getpid();  /* self pid */
+    nlh->nlmsg_flags = 0;
+
+    char* my_msg = get_message();
+    /* Fill in the netlink message payload */
+    strcpy(NLMSG_DATA(nlh), my_msg);
+
+    memset(&iov, 0, sizeof(iov));
+    iov.iov_base = (void *)nlh;
+    iov.iov_len = nlh->nlmsg_len;
+
+    memset(&msg, 0, sizeof(msg));
+    msg.msg_name = (void *)&dest_addr;
+    msg.msg_namelen = sizeof(dest_addr);
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+
+    printf("Send to kernel: %s\n", my_msg);
+
+    rc = sendmsg(sock_fd, &msg, 0);
+    if (rc < 0) {
+        printf("sendmsg(): %s\n", strerror(errno));
+        close(sock_fd);
+        return 1;
+    }
+
+    /* Read message from kernel */
+    memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
+
+    rc = recvmsg(sock_fd, &msg, 0);
+    if (rc < 0) {
+        printf("sendmsg(): %s\n", strerror(errno));
+        close(sock_fd);
+        return 1;
+    }
+
+    printf("Received from kernel: %s\n", NLMSG_DATA(nlh));
+    free(nlh);
+    }
+  /* Close Netlink Socket */
+  close(sock_fd);
+
+  return 0;
+}
